3b76630571948cb4096789a656e1c81cbdf04538
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2021-12-15 18:36:53 -0300

    Adding tests on ClientRepository

---
+++ dsclient/src/test/java/com/devsuperior/dsclient/repositories/ClientRepositoryTests.java
@@ -0,0 +1,78 @@
+package com.devsuperior.dsclient.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dsclient.entity.Client;
+import com.devsuperior.dsclient.tests.Factory;
+
+@DataJpaTest
+public class ClientRepositoryTests {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalElements;
+	private Client client;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		client = Factory.createClient();
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalElements = 20L;
+	}
+	
+	@Test
+	public void findAllShouldReturnAllClients() {
+		
+		List<Client> list = repository.findAll();
+		
+		Assertions.assertNotNull(list);
+	}
+	
+	@Test
+	public void findByIdShouldReturnOptionalClientWhenIdExist() {
+		
+		Optional<Client> obj = repository.findById(existingId);
+		
+		Assertions.assertTrue(obj.isPresent());
+		Assertions.assertNotNull(obj.get());
+	}
+	
+	@Test
+	public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExist() {
+		
+		Optional<Client> obj = repository.findById(nonExistingId);
+		
+		Assertions.assertTrue(obj.isEmpty());
+	}
+	
+	@Test 
+	public void saveShouldSaveObjectAndReturnObject() {
+		
+		client.setId(null);
+		client = repository.save(client);
+		
+		Assertions.assertNotNull(client);
+		Assertions.assertEquals(countTotalElements + 1, client.getId());
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExist() {
+		
+		repository.deleteById(existingId);
+		Optional<Client> obj = repository.findById(existingId);
+		
+		Assertions.assertTrue(obj.isEmpty());
+	}
+
+}
---
+++ dsclient/src/test/java/com/devsuperior/dsclient/tests/Factory.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dsclient.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entity.Client;
+
+public class Factory {
+
+	public static Client createClient() {
+		return new Client(1L, "Sonia Braga", "233.256.459-16", 2330.0, Instant.parse("2021-09-22T00:00:00Z"), 3);
+	}
+	
+	public static ClientDTO createClientDTO() {
+		return new ClientDTO(createClient());
+	}
+}
