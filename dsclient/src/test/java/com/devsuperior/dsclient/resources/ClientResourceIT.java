0312542a83e46ace2679d9e57fff6dab7b16ca58
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2021-12-17 10:46:24 -0300

    Implementing IT(Integration Testing)

---
+++ dsclient/src/test/java/com/devsuperior/dsclient/resources/ClientResourceIT.java
@@ -0,0 +1,159 @@
+package com.devsuperior.dsclient.resources;
+
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.services.ClientService;
+import com.devsuperior.dsclient.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ClientResourceIT {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalClients;
+	private ClientDTO clientDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		nonExistingId = 100L;
+		countTotalClients = 20L;
+		clientDTO = Factory.createClientDTO();
+	}
+	
+
+	@Test
+	public void findAllShouldReturnPageSortedByName() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/clients?orderBy=name")
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.content[0].name").value("Alex Green"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/clients")
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExist() throws Exception{
+		
+		String jsonBody = objectMapper.writeValueAsString(clientDTO);
+		
+		ResultActions result = mockMvc.perform(delete("/clients/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deleteShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+		
+		String jsonBody = objectMapper.writeValueAsString(clientDTO);
+		
+		ResultActions result = mockMvc.perform(delete("/clients/{id}", nonExistingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+		
+		String jsonBody = objectMapper.writeValueAsString(clientDTO);
+		
+		ResultActions result = mockMvc.perform(put("/clients/{id}", nonExistingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
+	
+	
+	@Test
+	public void updateShouldUpdateClientWhenIdExist() throws Exception{
+		
+		String jsonBody = objectMapper.writeValueAsString(clientDTO);
+		
+		ResultActions result = mockMvc.perform(put("/clients/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(existingId));
+	}
+	
+	@Test
+	public void insertShouldInsertClient() throws Exception{
+		
+		String jsonBody = objectMapper.writeValueAsString(clientDTO);
+		
+		ResultActions result = mockMvc.perform(post("/clients")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(countTotalClients + 1));
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/clients/{id}", nonExistingId).accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+
+	}
+	
+	@Test
+	public void findByIdShouldReturnClientWhenIdExist() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/clients/{id}", existingId).accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(existingId));
+	}
+}
---
+++ dsclient/src/test/java/com/devsuperior/dsclient/services/ClientServiceIT.java
@@ -0,0 +1,127 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entity.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsclient.tests.Factory;
+
+@SpringBootTest
+@Transactional
+public class ClientServiceIT {
+	
+	@Autowired
+	private ClientService service;
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private ClientDTO clientDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		nonExistingId = 100L;
+		clientDTO = Factory.createClientDTO();
+	}
+	
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ClientDTO> page = service.findAllPaged(pageRequest);
+	
+		Assertions.assertTrue(page.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ClientDTO> page = service.findAllPaged(pageRequest);
+	
+		Assertions.assertFalse(page.isEmpty());
+	}
+	
+	@Test
+	public void deleteShouldDeleteClientDTOWhenIdExist() {
+		
+		service.delete(existingId);
+		
+		Optional<Client> result = repository.findById(existingId);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+	}
+	
+	@Test
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingId, clientDTO);
+		});
+		
+	}
+	
+	@Test
+	public void updateShouldUpdateClientDTOWhenIdExist() {
+		
+		clientDTO = service.update(existingId, clientDTO);
+		
+		Assertions.assertNotNull(clientDTO);
+		Assertions.assertEquals(existingId, clientDTO.getId());
+	}
+	
+	@Test
+	public void insertShouldInsertObject() {
+		
+		clientDTO = service.insert(clientDTO);
+		
+		Assertions.assertNotNull(clientDTO);
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
+		
+	}
+	
+	@Test
+	public void findByIdShouldReturnClientDTOWhenIdExist() {
+		
+		ClientDTO dto = service.findById(existingId);
+		
+		Assertions.assertEquals(existingId, dto.getId());
+		Assertions.assertNotNull(dto);
+		
+	}
+}
