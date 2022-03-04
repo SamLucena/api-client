3f7e0270e729b33a1d02d360fb5da1d5561dc3c4
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2022-03-04 10:25:20 -0300

    pipeline:main-to-homolog

--- dsclient/src/main/java/com/devsuperior/dsclient/DsclientApplication.java
+++ dsclient/src/main/java/com/devsuperior/dsclient/DsclientApplication.java
@@ -2,7 +2,9 @@ package com.devsuperior.dsclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * author: SamLucena
+ * */
 @SpringBootApplication
 public class DsclientApplication {
 
